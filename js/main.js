c3d8ad138b1413cef1093579e254d0088d3fc345
saman-jafari <isaimon@me.com>
2018-07-30 01:02:32 +0200

    all functions are working

--- js/main.js
+++ js/main.js
@@ -5,38 +5,54 @@ window.addEventListener('load', function () {
     const bs = document.getElementById('backspace');
     const divide = document.getElementById('divide');
     const times = document.getElementById('times');
-    const minus = document.getElementById('minus');
+    const sub = document.getElementById('minus');
     const add = document.getElementById('plus');
     const pm = document.getElementById('pm');
     const eq = document.getElementById('eq');
     const num = document.getElementsByClassName('num');
     const output = document.getElementById('output');
-
+    let action;
+    let actionEq;
     let val = null;
     let def = 0;
+    output.addEventListener('', function () {
+
+    });
 
     /**
      * get numbers
      */
     for (let i = 0; i < num.length; i++) {
+
         num[i].addEventListener('click', function (e) {
+            if (action !== 'ADD' && action !== 'SUB' && action !== 'DIVIDE' && action !== 'TIMES' && action !== "EQ") {
                 def = e.target.value;
                 output.value += def;
+            } else {
+                output.value = e.target.value;
+                action = null;
+            }
         });
-        num[i].addEventListener('change', function (e) {
 
-        })
     }
+    /**
+     * plus minus
+     */
+    pm.addEventListener('click', function () {
+        if ( output.value < 0 || output.value > 0) {
+            output.value = -1 * output.value
+        }
+    });
     /**
      * delete all
      */
     c.addEventListener('click', function () {
         output.value = null;
-        firstVal = null;
-        secVal = null;
+        val = null;
+        actionEq = null;
     });
-    output.addEventListener('change', function () {
-
+    ce.addEventListener('click', function () {
+        output.value = null;
     });
 
     /**
@@ -44,24 +60,90 @@ window.addEventListener('load', function () {
      */
     bs.addEventListener('click', function () {
         let outputArr = output.value.split('');
-        if (!outputArr.length <= 1 ){
-        outputArr.pop();
-        output.value = outputArr.join('');
+        if (!outputArr.length <= 1) {
+            outputArr.pop();
+            output.value = outputArr.join('');
         } else {
             output.value = 0;
+        }
+    });
+
+
+    add.addEventListener('click', function () {
+        action = 'ADD';
+        actionEq = 'ADD';
+        if (val === null) {
+            val = parseFloat(output.value);
+            output.value = null;
+        } else {
+            val = parseFloat(output.value) + val;
+            output.value = val
+        }
+    });
+    sub.addEventListener('click', function () {
+        action = 'SUB';
+        actionEq = 'SUB';
+        if (val === null) {
+            val = parseFloat(output.value);
+            output.value = null;
+        } else {
+            val = parseFloat(output.value) - val;
+            output.value = val
+        }
+    });
+    divide.addEventListener('click', function () {
+        action = 'DIVIDE';
+        actionEq = 'DIVIDE';
+        if (val === null) {
+            val = parseFloat(output.value);
+            output.value = null;
+        } else {
+            val = parseFloat(output.value) / val;
+            output.value = val
+        }
+    });
+    times.addEventListener('click', function () {
+        action = 'TIMES';
+        actionEq = 'TIMES';
+        if (val === null) {
+            val = parseFloat(output.value);
+            output.value = null;
+        } else {
+            val = parseFloat(output.value) * val;
+            output.value = val
         }
     });
     /**
      * equal
      */
-    eq.addEventListener('click',function () {
+    eq.addEventListener('click', function () {
+        switch (actionEq) {
+            case "ADD":
+                val = parseFloat(output.value) + val;
+                output.value = val;
+                action = 'EQ';
+                break;
+            case "SUB":
+                val = parseFloat(output.value) - val;
+                output.value = val;
+                action = 'EQ';
+                break;
+            case "DIVIDE":
+                val = parseFloat(output.value) / val;
+                output.value = val;
+                action = 'EQ';
+                break;
+            case "TIMES":
+                val = parseFloat(output.value) * val;
+                output.value = val;
+                action = 'EQ';
+                break;
+            default:
+                output.value = null;
+                action = 'EQ';
+                break;
+
+        }
 
     });
-
-    add.addEventListener('click',function () {
-        val = parseFloat(output.value)
-        val += val
-        output.value = val
-
-    })
 });
